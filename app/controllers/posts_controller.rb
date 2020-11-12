9d7bff939d298289e44fd35e8d49eb57ffb56468
rloterh <rloterh@gmail.com>
2020-11-12 21:52:16 +0000

    Refactor show route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,9 @@ class PostsController < ApplicationController
 
   # GET /posts/1
   # GET /posts/1.json
-  def show; end
+  def show
+    @posts = Post.find(params[:id])
+  end
 
   # GET /posts/new
   def new
