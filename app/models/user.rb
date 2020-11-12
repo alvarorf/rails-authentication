419aa02ff3f97eb96765e4086c38d478cf167f40
alvaro <aaruizf@unal.edu.co>
2020-11-12 12:22:10 -0500

    Add actions to users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :username, presence: true, uniqueness: true, length: { in: 4..200 }
   validates :email, presence: true, uniqueness: true, length: { in: 10..254 }, format:
   {
     with: URI::MailTo::EMAIL_REGEXP
   }
   validates :password, presence: true, length: { in: 8..30 }
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
