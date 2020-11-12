ff667977638d0f03c1c14fe4a3c8851d671c5c9f
rloterh <rloterh@gmail.com>
2020-11-12 15:59:00 +0000

    Rollback user db

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_150031) do
+ActiveRecord::Schema.define(version: 2020_11_12_150019) do
 
   create_table "posts", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
