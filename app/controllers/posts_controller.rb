5932c3f316fcc76b88f7482df298fb60d1167d3f
rloterh <rloterh@gmail.com>
2020-11-12 22:54:38 +0000

    Refactor post form and action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -71,6 +71,7 @@ class PostsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def post_params
-    params.fetch(:post, {})
+    params.require(:post).permit(:title, :body)
+
   end
 end
