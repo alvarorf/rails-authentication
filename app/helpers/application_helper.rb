cbab729fe09c1285a882c8dc5a5ff5ea568e8a9a
alvaro <aaruizf@unal.edu.co>
2020-11-13 14:17:35 -0500

    Remove logic from application view and moved it to the application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def username_if_log_in(current_user)
+    if user_signed_in?
+      "Signed-in as: <strong>#{current_user.username}</strong>  ".html_safe
+    else
+      link_to 'Log in', new_user_session_path unless user_signed_in?
+    end
+  end
+
+  def sign_up
+    link_to 'Sign Up' unless user_signed_in?
+  end
+
+  def log_out
+    return link_to 'Log out' if user_signed_in?
+  end
+
+  def new_post
+    return link_to 'New Post', new_post_path if user_signed_in?
+  end
+
+
 end
