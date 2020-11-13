6a701c2fdb5fc77acd1c49e032ed31191ae55c54
alvaro <aaruizf@unal.edu.co>
2020-11-12 23:10:37 -0500

    Solve issue while creating a new post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,6 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:username,:email,:password,:pass_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
