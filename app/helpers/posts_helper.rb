3c3d34103b01f2a0e794d6de186a9b9430c69f4b
alvaro <aaruizf@unal.edu.co>
2020-11-13 13:33:39 -0500

    Remove view logic from post/index.html.erb, add helper methods (as suggested by TSE) and solve issue that we had while trying to create a post

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,18 @@ module PostsHelper
   def edit_button(post)
     link_to 'Edit', edit_post_path(post) if user_signed_in?
   end
+
+  def username(post)
+    if user_signed_in?
+      post.user.username
+    else
+      'You need to sign-in to see this.'
+    end
+  end
+
+  def destroy_button(post)
+    if user_signed_in?
+      link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }, class: 'btn btn-danger text-white mb-2'
+    end
+  end
 end
