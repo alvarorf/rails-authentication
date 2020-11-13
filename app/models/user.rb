e701ec1c5b914c146b3aa726a5eeac5444430c14
alvaro <aaruizf@unal.edu.co>
2020-11-13 13:15:41 -0500

    Delete some debugging code and add some css rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :username, presence: true, uniqueness: true, length: { in: 5...254 }
   validates :email, presence: true, uniqueness: true, length: { in: 10..254 }, format:
   {
